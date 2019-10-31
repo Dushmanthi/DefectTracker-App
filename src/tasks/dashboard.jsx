866d9aed05f4b0d3dbd2f372cfce73ed4587cde6
Dushmanthi Fernando <madushanimf@gmail.com>
2019-10-31 16:39:22 +0530

    add dashboard

---
+++ src/tasks/dashboard.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Card,CardDeck,CardBody,CardImg } from "react-bootstrap";
+import './dashboard.css';
+
+class Dashboard extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div class='body'>
+  
+
+         </div>
+         );
+    }
+}
+ 
+export default Dashboard;
