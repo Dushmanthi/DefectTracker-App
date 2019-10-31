1ad16ba094f970302d3c63e502d62d0fc577d991
Dushmanthi Fernando <madushanimf@gmail.com>
2019-10-31 16:30:32 +0530

    create dashboard

---
+++ src/dashboard.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Card,CardDeck,CardBody,CardImg } from "react-bootstrap";
+import './dashboard.css';
+
+class Dashboard extends Component {
+    state = {  }
+    render() { 
+        return ( );
+     }
+}
+ 
+export default Dashboard;       
